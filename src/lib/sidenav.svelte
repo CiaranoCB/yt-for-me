758c1903eec9a488614813b543923da217c2d8ab
ciaranocb <ciaranogj@gmail.com>
2023-09-27 18:29:04 -0400

    initial commit

---
+++ src/lib/sidenav.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  // creates a bindinnng that let's outside 
+  // components influence the state here
+  export let show = true;
+  export let isMobile = false;
+</script>
+
+{#if show}
+    <section class="bg-slate-900">
+      SideNav that should hide on small screen
+    </section>
+{/if}
+
+
+<style>
+  section {
+    width: 300px;
+  }
+  .fixed {
+    display: fixed;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,54 @@
+<script>
+    import Thumb from "$lib/thumb.svelte";
+    import { onMount } from "svelte";
+    let loading = true;
+    onMount(() => {
+        setTimeout(() => {
+            // where the code runs for the timeout
+            loading = false;
+        }, 5000);
+    })
+</script>
+
+<hi></hi>
+
+<div class="flex subscription justify-between px-8 flex-wrap">
+    <Thumb
+    title = "BIG FOOT SIGHTINGS!!!"
+    thumbnail = "Feet and Soil.jpeg"
+    avatar = "photo2.jpeg"
+    creator = "Jonny jpeg"
+    views = ""
+    loading={loading}
+    />
+    <Thumb
+    title = "Grass and McDonald Sprite River!"
+    thumbnail = "Grass and Water.jpeg"
+    avatar = "photo2.jpeg"
+    creator = "Jonny jpeg"
+    views = ""
+    loading={loading}
+    />
+    <Thumb />
+    <Thumb />
+
+
+
+    <Thumb />
+    <Thumb />
+    <Thumb />
+    <Thumb />
+
+
+
+    <Thumb />
+    <Thumb />
+    <Thumb />
+    <Thumb />
+</div>
+
+<style>
+    .flex {
+        padding-bottom: 4rem;
+    }
+</style>
